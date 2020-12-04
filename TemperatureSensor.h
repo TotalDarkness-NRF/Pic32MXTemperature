9f7d50b456178e69d04e8f5c5862c098c7bb54dd
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2020-12-04 13:38:02 -0500

    Add defines for commands for sensor

--- TemperatureSensor.h
+++ TemperatureSensor.h
@@ -1,9 +1,31 @@
 #ifndef TEMPERATURE_SENSOR_H_INCLUDED
 #define TEMPERATURE_SENSOR_H_INCLUDED
 
+/*
+ * Below defines pins for sensor
+ */
 #define PIN_DIRECTION 	_TRISA0
 #define WRITE_PIN  		_LATA0
 #define READ_PIN		_RA0
+
+/*
+ * Below defines commands set for sensor
+ */
+#define CONVERT_T                 0x44
+#define READ_SCRATCHPAD     0xBE
+#define WRITE_SCRATCHPAD    0x4E
+#define COPY_SCRATCHPAD     0x48
+#define RECALL_E                     0xB8
+#define READ_POWER               0xB4
+
+/*
+ * Below defines ROM commands
+ */
+#define SEARCH_ROM              0xF0
+#define READ_ROM                  0x33
+#define MATCH_ROM                0x55
+#define SKIP_ROM                    0xCC
+#define ALARM_SEARCH            0xEC
 
 void DriveOW(unsigned char Bit);
 int ResetPulse();
