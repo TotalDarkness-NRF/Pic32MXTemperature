5e29d8d1e181b69699a8ebbf1ed4118883261aa3
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2020-12-03 22:57:50 -0500

    note for interruptions

--- Buttons.c
+++ Buttons.c
@@ -1,9 +1,8 @@
 #include <p32xxxx.h>
 #include "Buttons.h"
 #include "LCD.h"
-#include "TemperatureSensor.h"
 
-void Color() {
+void Color() { // TODO maybe have interupts instead, delay do to getting temperature
         if (BUTTON4) {
             RED = !RED;
             Delay(200);
