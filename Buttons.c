15c0a7541f4d6acc5e46c1d7a01e743235d7a4db
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2020-12-05 02:37:36 -0500

    Add button3 changing precision

--- Buttons.c
+++ Buttons.c
@@ -1,6 +1,8 @@
 #include "Thermometer.h"
 
-void Color() { // TODO maybe have interupts instead, delay do to getting temperature
+ // TODO maybe have interupts instead, delay do to getting temperature
+
+void Color() {
         if (BUTTON4) {
             RED = !RED;
             Delay(200);
@@ -14,4 +16,26 @@ void Color() { // TODO maybe have interupts instead, delay do to getting tempera
             BLUE = !BLUE;
             Delay(200);
         }
+}
+
+void ChangePrecision() {
+    if(BUTTON3) {
+        int precision = getPrecsion();
+        if (precision >= 12) 
+            setPrecision(9);
+        else setPrecision(++precision);
+         clearDisplay();
+         char text[32];
+         sprintf(text, "Precision set to%D", getPrecsion());
+         displayString(text);
+         Delay(1000);
+    }
+}
+
+void ChangeTemperatureUnit() {
+    
+}
+
+void ShowTime() {
+    
 }
