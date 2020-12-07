50a30a754bf2cf83948fae64b208c4dc14a3d25e
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2020-12-06 23:17:19 -0500

    Reformat checks

--- Buttons.c
+++ Buttons.c
@@ -9,7 +9,6 @@ void Color() {
             RED = !RED;
             Delay(200);
         }
-        
         if (BUTTON5) {
             GREEN = !GREEN;
             Delay(200);
@@ -26,8 +25,10 @@ void ChangePrecision() {
         precision = precision >= 12 ? 9 : ++precision;
         setPrecision(precision);
         clearDisplay();
-        sprintf(text, "Precision set to%D", precision);
+        sprintf(text, "Precision set to");
         displayString(text);
+        sprintf(text, "%d", precision);
+        displayStringStart(24, text);
         Delay(1000);
         clearDisplay();
     }
@@ -36,22 +37,14 @@ void ChangePrecision() {
 void ChangeTemperatureUnit() {
     if (BUTTON3) {
         char unit = getUnit();
-        if (unit == 'F') {
-            unit = 'K';
-        } else if (unit == 'C') {
-            unit = 'F';
-        } else {
-            unit = 'C';
-        }
+        if (unit == 'F') unit = 'K';
+        else if (unit == 'C') unit = 'F';
+        else unit = 'C';
         setUnit(unit);
         clearDisplay();
-        if (unit == 'K') {
-            sprintf(text, "Temperature set to Kelvin");
-        } else if (unit == 'F') {
-            sprintf(text, "Temperature set to Fahrenheit");
-        } else {
-            sprintf(text, "Temperature set to Celsius");
-        }
+        if (unit == 'K') sprintf(text, "Temperature set to Kelvin");
+        else if (unit == 'F') sprintf(text, "Temperature set to Fahrenheit");
+        else sprintf(text, "Temperature set to Celsius");
         displayString(text);
         Delay(1000);
         clearDisplay();
